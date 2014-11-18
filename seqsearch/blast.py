e6c91f9489a0fb175f90f5a91825f5a5083ba4a2
Lucas Sinclair <lucas.sinclair@me.com>
2014-11-18 21:53:48 +0100

    seq_type (bis)

--- seqsearch/blast.py
+++ seqsearch/blast.py
@@ -131,7 +131,7 @@ class BLASTdb(FASTA):
     def __repr__(self): return '<%s on "%s">' % (self.__class__.__name__, self.path)
 
     def __init__(self, fasta_path, seq_type='nucl' or 'prot'):
-        if hasattr(fasta_path, 'seq_type'): self.seq_type = self.fasta_path.seq_type
+        if hasattr(fasta_path, 'seq_type'): self.seq_type = fasta_path.seq_type
         else:                               self.seq_type = seq_type
         FASTA.__init__(self, fasta_path)
 
