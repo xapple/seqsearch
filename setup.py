a060c93623ef6f4e676d5d56d07118e6d1ca41cd
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-09-03 22:55:57 +0200

    Version 1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'seqsearch',
-    version          = '1.3.0',
+    version          = '1.3.1',
     description      = 'Sequence similarity searches (e.g. BLAST) made easy.',
     license          = 'MIT',
     url              = 'https://github.com/xapple/seqsearch',
