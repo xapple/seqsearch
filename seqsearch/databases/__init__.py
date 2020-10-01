9fc8723973b072e656bf12ca102ad2856460c867
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-10-01 17:38:01 +0200

    Add the NCBI ribosomal 16S database

--- seqsearch/databases/__init__.py
+++ seqsearch/databases/__init__.py
@@ -12,15 +12,16 @@ import os, fnmatch
 from collections import OrderedDict, Counter
 
 # First party modules #
-from autopaths.auto_paths import AutoPaths
-from autopaths.file_path import FilePath
-from plumbing.cache import property_cached
-from plumbing.common import natural_sort
 from fasta import FASTA
+from autopaths.auto_paths import AutoPaths
+from autopaths.dir_path   import DirectoryPath
+from autopaths.file_path  import FilePath
+from plumbing.cache       import property_cached
+from plumbing.common      import natural_sort
 
 # Third party modules #
 from ftputil import FTPHost
-from tqdm import tqdm
+from tqdm    import tqdm
 
 # Constants #
 home = os.environ.get('HOME', '~') + '/'
@@ -36,23 +37,27 @@ class Database:
     /blast_db/
     """
 
-    def __init__(self, seq_type, base_dir=None):
-        # Attributes #
+    def __init__(self, seq_type=None, base_dir=None):
+        # The sequence type is either 'prot' or 'nucl' #
         self.seq_type = seq_type
-        # Base directory #
-        if base_dir is None: base_dir = os.environ.get('HOME', '/') + '/'
+        # The default base directory #
+        if base_dir is None:
+            base_dir = os.environ.get('HOME', '/') + '/'
+        # Make base_dir object #
         self.base_dir = base_dir + 'databases/' + self.short_name + '/'
-        self.autopaths        = AutoPaths(self.base_dir, self.all_paths)
+        self.base_dir = DirectoryPath(self.base_dir)
+        # Make autopaths object #
+        self.autopaths = AutoPaths(self.base_dir, self.all_paths)
 
     @property_cached
     def ftp(self):
-        """If the data is to be obtained by FTP here is the ftputil object."""
+        """If the data is to be obtained by FTP, here is the ftputil object."""
         ftp = FTPHost(self.ftp_url, "anonymous")
         ftp.chdir(self.ftp_dir)
         return ftp
 
     @property_cached
-    def files_to_retrive(self):
+    def files_to_retrieve(self):
         """The files we want to download with their destinations."""
         if hasattr(self, "pattern"):
             files = self.ftp.listdir(self.ftp.curdir)
@@ -65,13 +70,15 @@ class Database:
     @property
     def files_remaining(self):
         """The files we haven't downloaded yet based on size checks."""
-        return OrderedDict((source,dest) for source,dest in self.files_to_retrive.items()
+        return OrderedDict((source,dest) for source, dest in self.files_to_retrieve.items()
                            if dest.count_bytes != self.ftp.path.getsize(source))
 
     def download(self):
         """Retrieve all files from the FTP site."""
+        # Create the directory #
         self.base_dir.create_if_not_exists()
-        for source,dest in tqdm(self.files_remaining.items()):
+        # Loop over files #
+        for source, dest in tqdm(self.files_remaining.items()):
             dest.remove()
             self.ftp.download(source, dest)
             dest.permissions.only_readable()
@@ -105,16 +112,21 @@ class Database:
     @property_cached
     def blast_db(self):
         """A BLASTable version of the sequences."""
+        # Import #
         from seqsearch.search.blast import BLASTdb
-        return BLASTdb(self.autopaths.unzipped_dir+self.short_name, 'nucl')
+        # Create object #
+        db = BLASTdb(self.autopaths.unzipped_dir + self.db_name,
+                     self.seq_type)
+        # Return #
+        return db
 
     #--------------------- Only for taxonomic databases ----------------------#
     @property_cached
     def tax_depth_freq(self):
         def depths():
-           with open(self.taxonomy, 'r') as handle:
-               for line in handle:
-                   line = line.strip('\n')
-                   otu_name, species = line.split('\t')
-                   yield len(species.split(';'))
+            with open(self.taxonomy, 'r') as handle:
+                for line in handle:
+                    line = line.strip('\n')
+                    otu_name, species = line.split('\t')
+                    yield len(species.split(';'))
         return Counter(depths())
---
+++ seqsearch/databases/ncbi_16s.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Built-in modules #
+import os
+
+# First party modules #
+from seqsearch.databases import Database
+
+# Third party modules #
+
+# Constants #
+home = os.environ.get('HOME', '~') + '/'
+
+###############################################################################
+class NCBI16S(Database):
+    """
+    The NCBI provides a database of 16S ribosomal sequences.
+    The exact title is "Bacteria and Archaea: 16S ribosomal RNA project".
+    More information is available at:
+
+    https://www.ncbi.nlm.nih.gov/refseq/targetedloci/16S_process/
+
+    To install:
+
+        >>> from seqsearch.databases.ncbi_16s import ncbi_16s
+        >>> ncbi_16s.download()
+        >>> ncbi_16s.untargz()
+
+    It will place the resulting files in ~/databases/ncbi_16s/
+    """
+
+    short_name = "ncbi_16s"
+    base_url   = "ftp://ftp.ncbi.nlm.nih.gov/blast/db/"
+    ftp_url    = "ftp.ncbi.nlm.nih.gov"
+    ftp_dir    = "/blast/db/"
+    files      = ['16S_ribosomal_RNA.tar.gz']
+    db_name    = '16S_ribosomal_RNA'
+
+###############################################################################
+ncbi_16s = NCBI16S('nucl')
