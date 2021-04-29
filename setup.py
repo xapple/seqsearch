f1b1e4a469b5ce361ae1e2ffa99964343d2a9e83
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-04-29 21:43:41 +0200

    Bump package to version 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name             = 'seqsearch',
-    version          = '1.3.4',
+    version          = '2.0.1',
     description      = 'Sequence similarity searches (e.g. BLAST) made easy.',
     license          = 'MIT',
     url              = 'https://github.com/xapple/seqsearch',
@@ -11,8 +11,7 @@ setup(
     classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
     packages         = find_namespace_packages(),
     install_requires = ['plumbing>=2.8.9', 'autopaths>=1.4.6', 'fasta>=2.1.0',
-                        'biopython', 'decorator', 'sh', 'tqdm', 'ftputil',
-                        'wget'],
+                        'biopython', 'sh', 'tqdm', 'ftputil', 'wget'],
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
     include_package_data = True,
