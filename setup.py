97e2fdbdbd396425d3af2d87b8268c89974d0a08
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-19 23:01:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from setuptools import setup, find_namespace_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
 setup(
     name             = 'seqsearch',
     version          = '2.0.5',
@@ -9,10 +26,11 @@ setup(
     author           = 'Lucas Sinclair',
     author_email     = 'lucas.sinclair@me.com',
     classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
-    packages         = find_namespace_packages(),
+    packages         = find_namespace_packages(exclude=['example']),
     install_requires = ['plumbing>=2.8.9', 'autopaths>=1.4.6', 'fasta>=2.1.0',
                         'biopython', 'sh', 'tqdm', 'ftputil', 'wget'],
-    long_description = open('README.md').read(),
+    python_requires  = ">=3.6",
+    long_description = readme,
     long_description_content_type = 'text/markdown',
     include_package_data = True,
 )
