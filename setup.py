966f2a3b889d1421c5ef5a7267df9c767c5bdb29
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-26 14:53:29 +0200

    Version 2.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
 # Call setup #
 setup(
     name             = 'seqsearch',
-    version          = '2.0.9',
+    version          = '2.1.0',
     description      = 'Sequence similarity searches (e.g. BLAST) made easy.',
     license          = 'MIT',
     url              = 'https://github.com/xapple/seqsearch',
@@ -27,7 +27,7 @@ setup(
     author_email     = 'lucas.sinclair@me.com',
     classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
     packages         = find_namespace_packages(exclude=['example']),
-    install_requires = ['plumbing>=2.10.4', 'autopaths>=1.4.12',
+    install_requires = ['autopaths>=1.5.0', 'plumbing>=2.10.4',
                         'fasta>=2.2.11',
                         'biopython', 'sh', 'tqdm'],
     extras_require   = {'ftp':       ['ftputil'],
